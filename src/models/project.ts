9b6b155f5240fefac75e32e964d04f1ccac0791e
Paul Pietzko <paul.pietzko@icloud.com>
2025-04-18 23:11:52 +0200

    feat: details pages for projects

---
+++ src/models/project.ts
@@ -0,0 +1,18 @@
+export interface Project {
+  id: number;
+  title: string;
+  cover_url: string;
+  short_text: string;
+  published_at: string;
+  views_count: number;
+  downloads_count: number;
+  url: string;
+  p_windows: boolean;
+  p_linux: boolean;
+  p_osx: boolean;
+  p_android: boolean;
+  type: string;
+  min_price: number;
+  classification: string;
+  earnings?: { amount_formatted: string; amount: number; currency: string }[];
+}
